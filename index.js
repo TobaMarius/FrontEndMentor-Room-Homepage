9a405f64e275a884ba6da7345b1626f7a50bca74
TobaMarius <mariustoba@protonmail.com>
2020-11-10 00:11:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+$( "#shop" ).click(function() {
+    alert( "Handler for shop called." );
+  });
+
+
+  $( "#about" ).click(function() {
+    alert( "Handler for about called." );
+  });
+
+  $( "#contact" ).click(function() {
+    alert( "Handler for contact called." );
+  });
+
+  $( "#home" ).click(function() {
+    alert( "This is home tab." );
+  });
